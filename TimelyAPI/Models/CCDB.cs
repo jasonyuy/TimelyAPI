008eda52872fcd6ac76996ab62b9c1e0dd4bd632
Julia Lin <linjulia@usc.edu>
2018-06-26 09:07:44 -0700

    Parse entities
    Got the parsing method to work for CCDB-Batch
    
    Fix as much as I can for CCDB-Sample tests
    
    Got IPFERM tests passing, but should improve the workaround later
    
    Fix as much as I can for MES, LIMS, TW, etc tests
    
    Modify tests - use a specific batch instead of a tank
    
    Rename CannedResponse1() and ProcessMessage1() and their original version
    
    Fix regex for numbers only
    
    Fix logic in detecting partial alias
    
    Update tests to use a lot that is in process
    
    Separate category and subcategory
    
    Get product and vessel class from database
    
    Attempt at Porter's stemming algorithm
    
    Instead of stemming, add to db as alias
    
    Entities for equipment, modifier, limit, etc now in db
    
    Simplify the algorithm for dealing with edge cases

--- TimelyAPI/Models/CCDB.cs
+++ TimelyAPI/Models/CCDB.cs
@@ -493,7 +493,8 @@ namespace TimelyAPI.Models
                 {"PRODUCT","PRODUCTID"},
                 {"PROCESS","PRODUCTID"},
                 {"SCALE","SCALEID"}, 
-                {"TANK","ISI.CCBATCHES.FERMID"}, 
+                {"TANK","ISI.CCBATCHES.FERMID"},
+                {"TANKS","ISI.CCBATCHES.FERMID"}, //ADDED
                 {"VESSEL","ISI.CCBATCHES.FERMID"}, 
                 {"EQUIPMENT","ISI.CCBATCHES.FERMID"}, 
                 {"FERM","ISI.CCBATCHES.FERMID"},
@@ -516,7 +517,8 @@ namespace TimelyAPI.Models
                 {"OFFLINE PH","PH"},
                 {"CO2","CO2"},
                 {"CARBON DIOXIDE","CO2"},
-                {" NA","NA"},
+                {" NA","NA"}, 
+                {"NA","NA"}, //ADDED
                 {"SODIUM","NA"},
                 {"NH4","NH4"},
                 {"AMMONIUM","NH4"},
@@ -526,6 +528,7 @@ namespace TimelyAPI.Models
                 {"OSMOLALITY","OSMOLALITY"},
                 {"ASGR","SLOPE"},
                 {"GROWTH RATE","SLOPE"},
+                {"GROWTH","SLOPE"}, //ADDED
                 {"IVPCV","IVPCV"},
                 {"IVCD","IVCD"},
                 {"SAMPLE","CAST((FROM_TZ(CAST(SAMPLETIME AS TIMESTAMP),'+00:00') AT TIME ZONE 'US/Pacific') AS DATE)"},
@@ -562,6 +565,7 @@ namespace TimelyAPI.Models
                 {"CO2","mmHg"},
                 {"CARBON DIOXIDE","mmHg"},
                 {" NA","mmol/L"},
+                {"NA","mmol/L"}, //ADDED
                 {"SODIUM","mmol/L"},
                 {"NH4","mmol/L"},
                 {"AMMONIUM","mmol/L"},
@@ -571,6 +575,7 @@ namespace TimelyAPI.Models
                 {"OSMOLALITY","mOsm/kg"},
                 {"ASGR","day-1"},
                 {"GROWTH RATE","day-1"},
+                {"GROWTH","day-1"}, //ADDED
                 {"IVPCV",""},
                 {"IVCD",""},
                 {"SAMPLE",""},
